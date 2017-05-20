15f3661d6eb5c4e97c73230d731e2617d80a6a3d
Fredrik Stromberg <fredrik314@gmail.com>
2017-05-20 17:06:35 +0100

    changed the api key header name to prefix Bearer

--- connexion/vendor/swagger-ui/swagger-ui.js
+++ connexion/vendor/swagger-ui/swagger-ui.js
@@ -3306,7 +3306,7 @@ ApiKeyAuthorization.prototype.apply = function (obj) {
     return true;
   } else if (this.type === 'header') {
     if(typeof obj.headers[this.name] === 'undefined') {
-      obj.headers[this.name] = this.value;
+      obj.headers[this.name] = "Bearer " + this.value;
     }
 
     return true;
